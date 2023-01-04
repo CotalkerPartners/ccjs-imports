31c95cf0993a0da5840c0f1706ae057d3b091546
Eduardo Soto <esotom92@gmail.com>
2023-01-04 17:33:54 -0300

    - baseapi and cotproperty

---
+++ BASE_API.js
@@ -0,0 +1,49 @@
+/* Arguments: axios, networkLogs [], errors []  */
+const handleError = (error) => {
+  const { response } = error
+  if (response.status === 404) {
+    errors.push(`NOT FOUND ${config.url}`)
+    return { error: null }
+  }
+  if (response.status === 401) {
+    throw new Error('NO AUTH PROVIDED YOU MUST SET useInternalAuth = true')
+  }
+  return { error: response.data }
+}
+
+const handleResult = (result, errors) => {
+  if (result.error) {
+    errors.push(result.error)
+    return null
+  }
+  return result.data
+}
+
+const _repeatableCall = async (path, method, data) => {
+  networkLogs.push(config)
+  const headers = { admin: 'true' }
+  if (method === 'POST' || method === 'PATCH') {
+    headers['Content-Type'] = 'application/json'
+  }
+  const config = {
+    url: `${env.EXTERNAL_API_URL}${path}`,
+    method,
+    headers,
+    data,
+  }
+  try {
+    const response = await axios(config)
+    return { data: response.data }
+  } catch (error) {
+    if (error.response.status === 429) {
+      const waitTime = Number(
+        error.response.headers['retry-after'] ?? error.response.headers['Retry-After'] ?? 1
+      )*1000
+      await new Promise(resolve => setTimeout(resolve, waitTime))
+      return _repeatableCall(path, method, data)
+    }
+    return handleError(error)
+  }
+}
+
+return { handleError, handleResult, _repeatableCall }
---
+++ COTProperty.js
@@ -0,0 +1,29 @@
+/* Arguments: baseAPI */
+const { _repeatableCall, handleError, handleResult } = baseAPI
+return {
+  getQuery: async (query) => {
+    const result = await _repeatableCall(`/api/v2/properties?${qs.stringify(query)}`, 'GET')
+    return handleResult(result, errors)
+  },
+  post: async (property) => {
+    const result = await _repeatableCall('/api/v2/properties', 'POST', property)
+    return handleResult(result, errors)
+  },
+  patch: async (id, body) => {
+    const result = await _repeatableCall(`/api/v2/properties/${id}`, 'PATCH', body)
+    return handleResult(result, errors)
+  },
+  jsonPatch: async (id, body) => {
+    if (!Array.isArray(body)) throw new Error('JSON PATCH MUST BE CALLED WITH AN ARRAY AS BODY')
+    const result = await _repeatableCall(`/api/v2/properties/jsonpatch/${id}`, 'PATCH', body)
+    return handleResult(result, errors)
+  },
+  getByCode: async (code) => {
+    const result = await _repeatableCall(`/api/v2/properties/code/${code}`, 'GET')
+    return handleResult(result, errors)
+  },
+  getById: async (id) => {
+    const result = await _repeatableCall(`/api/v2/properties/${id}`, 'GET')
+    return handleResult(result, errors)
+  }
+}
